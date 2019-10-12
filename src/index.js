30a11e7ef68565830336f01ee2aca55094e92dd0
Alan Vinicius Silva <alanvinicios@gmail.com>
2019-10-12 12:04:55 -0300

    Teste com express

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-var snmp = require('snmp-native');
+const express = require('express');
 
-const snmp_host = process.env.SNMP_HOST;
-const snmp_community = process.env.SNMP_COMMUNITY;
-const snmp_sysname_oid = process.env.SNMP_SYSNAME_OID;
-const snmp_porta1_oid = process.env.SNMP_PORTA1_OID;
-const snmp_porta2_oid = process.env.SNMP_PORTA2_OID;
+const app = express();
 
-var session = new snmp.Session({ host: snmp_host, community: snmp_community});
-
-
-session.get({ oid: snmp_porta2_oid }, function (error, varbinds) {
-    if (error) {
-        console.log('Fail :(');
-    } else {
-        console.log(varbinds[0].oid + ' = ' + varbinds[0].value + ' (' + varbinds[0].type + ')');
-    }
+app.get('/', (req, res) => {
+    res.send('Hello F');
 });
+
+app.listen(5000);
+
+
+
+
+// var snmp = require('snmp-native');
+
+// const snmp_host = process.env.SNMP_HOST;
+// const snmp_community = process.env.SNMP_COMMUNITY;
+// const snmp_sysname_oid = process.env.SNMP_SYSNAME_OID;
+// const snmp_porta1_oid = process.env.SNMP_PORTA1_OID;
+// const snmp_porta2_oid = process.env.SNMP_PORTA2_OID;
+
+// var session = new snmp.Session({ host: snmp_host, community: snmp_community});
+
+
+// session.get({ oid: snmp_porta2_oid }, function (error, varbinds) {
+//     if (error) {
+//         console.log('Fail :(');
+//     } else {
+//         console.log(varbinds[0].oid + ' = ' + varbinds[0].value + ' (' + varbinds[0].type + ')');
+//     }
+// });
