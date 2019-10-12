75cb38044b0f26f96fa0922fa7f867fc68d86690
Alan Vinicius Silva <alanvinicios@gmail.com>
2019-10-12 11:15:52 -0300

    Teste com o módulo snmp-native

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+var snmp = require('snmp-native');
+
+const snmp_host = process.env.SNMP_HOST;
+const snmp_community = process.env.SNMP_COMMUNITY;
+const snmp_sysname_oid = process.env.SNMP_SYSNAME_OID;
+const snmp_porta1_oid = process.env.SNMP_PORTA1_OID;
+const snmp_porta2_oid = process.env.SNMP_PORTA2_OID;
+
+var session = new snmp.Session({ host: snmp_host, community: snmp_community});
+
+
+session.get({ oid: snmp_sysname_oid }, function (error, varbinds) {
+    if (error) {
+        console.log('Fail :(');
+    } else {
+        console.log(varbinds[0].oid + ' = ' + varbinds[0].value + ' (' + varbinds[0].type + ')');
+    }
+});
