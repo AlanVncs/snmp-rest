954ee4033129bd3f469a97da14a12aa59f27e0b4
Alan Vinicius Silva <alanvinicios@gmail.com>
2019-10-13 14:13:30 -0300

    Regex compatível com a especificação

--- src/routes/snmpRouter.js
+++ src/routes/snmpRouter.js
@@ -1,8 +1,10 @@
 var router = require('express').Router();
 var snmpController = require('../controllers/snmpController');
 
-// /porta{N > 0}
-router.get(/^\/porta([0-9]*[1-9]+[0-9]*)$/, (req, res, next) => {
+// Números de 00 a 99 (Necessariamento com 2 dígitos)
+const portaRegex = /\/porta([0-9][1-9] | [1-9][1-9])/
+
+router.get(portaRegex, (req, res, next) => {
     const portaID = req.params[0];
     snmpController.getPorta(portaID, res);
 });
