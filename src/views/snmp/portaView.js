fba72d9283d4b7d17972fa3fe03cc423546e961a
Alan Vinicius Silva <alanvinicios@gmail.com>
2019-10-13 17:19:58 -0300

    Nenhuma mudança

--- src/views/snmp/portaView.js
+++ src/views/snmp/portaView.js
@@ -1,5 +1,3 @@
-// statusPortas = [1-up, 2-down, 3-testing, 4-unknown, 5-dormant, 6-notPresent, 7-lowerLayerDown)];
-
 const status = {'SUCCESS': 'success', 'FAIL': 'fail'};
 const states = {'1' : 'up', '2' : 'down', '3' : 'testing', '4' : 'unknown', '5' : 'dormant', '6' : 'notPresent', '7' : 'lowerLayerDown'};
 
