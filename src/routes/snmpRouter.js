99fe07b73ea8a4170ef63481fc11c04b19242225
Alan Vinicius Silva <alanvinicios@gmail.com>
2019-10-13 14:16:42 -0300

    Correção da regex

--- src/routes/snmpRouter.js
+++ src/routes/snmpRouter.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var snmpController = require('../controllers/snmpController');
 
 // Números de 00 a 99 (Necessariamento com 2 dígitos)
-const portaRegex = /\/porta([0-9][1-9] | [1-9][1-9])/
+const portaRegex = /\/porta([0-9][1-9]|[1-9][1-9])/
 
 router.get(portaRegex, (req, res, next) => {
     const portaID = req.params[0];
